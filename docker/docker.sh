171608157f22d2abe813ad1591ca13abc59c407d
Adrian Cederberg <adrn.cederberg123@gmail.com>
2024-08-01 16:28:14 -0600

    feature(deploy): Docker Quarto Build
    Before it was just mounting existing builds because ``build`` was not in
    ``.dockerignore``.
    Added logfile for ``docker.sh``.
    Removed ``demo.qmd``.
    Better css, in particular made animation inputs less disgusting.

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,7 +1,9 @@
 # NOTE: Get python dependencies.
 
 if [[ ! $ACEDERBERG_IO_VENV ]]; then export ACEDERBERG_IO_VENV="/home/docker/.venv"; fi
-if [[ ! $ACEDERBERG_IO_WORKDIR ]]; then export ACEDERBERG_IO_WORKDIR="/home/docker"; fi
+if [[ ! $ACEDERBERG_IO_WORKDIR ]]; then export ACEDERBERG_IO_WORKDIR="/home/docker/app"; fi
+if [[ ! $ACEDERBERG_IO_BUILD_LOG ]]; then export ACEDERBERG_IO_BUILD_LOG="/dev/null"; fi
+
 export PATH="$PATH:/home/docker/.local/bin"
 
 mkdir --parents $ACEDERBERG_IO_VENV
@@ -15,7 +17,9 @@ fi
 
 source $ACEDERBERG_IO_VENV/bin/activate
 
-python3.10 -m pip install poetry >>/dev/null
+python3.10 -m pip install poetry >>$ACEDERBERG_IO_BUILD_LOG
 
-cd $ACEDERBERG_IO_WORKDIR/app
-poetry install --no-root >>/dev/null
+cd $ACEDERBERG_IO_WORKDIR
+poetry install --no-root >>$ACEDERBERG_IO_BUILD_LOG
+
+which python3.10
