ddc311de0d52326c603205336c7610b8f59600b0
Adrian Cederberg <adrn.cederberg123@gmail.com>
2024-09-03 13:43:14 -0600

    feature(kaggle): Started Playing with Zillow Price Index Data.
    Decided there was not enought to do with it at the moment.

---
+++ kaggle/zillow/index.py
@@ -0,0 +1,45 @@
+import contextlib
+import io
+import pathlib
+
+import kagglehub
+import pandas as pd
+from pandas.api.types import CategoricalDtype
+
+DATASET_ID = "robikscube/zillow-home-value-index"
+
+
+# --------------------------------------------------------------------------- #
+# start snippet setup
+
+
+def get_data():
+    console = io.StringIO()
+    with contextlib.redirect_stdout(console), contextlib.redirect_stderr(console):
+        p = pathlib.Path(kagglehub.dataset_download(DATASET_ID))
+
+    return p, console
+
+
+def load_data(data_dir: pathlib.Path) -> pd.DataFrame:
+    data_dir, _ = get_data()
+    data_path = data_dir / "ZHVI.parquet"
+    return pd.read_parquet(data_path)
+
+
+def clean_data(data: pd.DataFrame) -> pd.DataFrame:
+
+    data.columns = [column.lower().replace(" ", "_") for column in data.columns]
+    data.index = pd.to_datetime(data.index)
+    data["year"] = data.index.year
+    data["month"] = data.index.month
+
+    return data
+
+
+def split_data(data: pd.DataFrame):
+    """Want to try to predict the final 5 years of data."""
+    return data[:"2019-07-01"], data["2019-07-01":]
+
+
+# end snippet setup
