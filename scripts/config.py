2a09465ef707e26b37ac599843c0f0b8cb3e9671
Adrian Cederberg <adrn.cederberg123@gmail.com>
2024-10-04 21:06:27 -0600

    feature(actions): Added Preview Site.
    Added ``ci.preview.yaml`` and preview routes.
    Added info page ``./blog/build.qmd`` using ``quarto`` variables.

--- scripts/config.py
+++ scripts/config.py
@@ -1,4 +1,5 @@
 import pathlib
+from datetime import datetime
 import logging
 import sys
 import yaml
@@ -8,36 +9,90 @@ from scripts import env
 logger = logging.getLogger(__name__)
 
 QUARTO = env.BLOG / "_quarto.yaml"
+QUARTO_VARIABLES = env.BLOG / "_variables.yaml"
 
 
-def main(quarto: pathlib.Path = QUARTO, _google_tracking_id: str | None = None, _dry: str = "1",):
+class Context:
+    git_commit: str
+    git_ref: str
+    quarto_variables: pathlib.Path
+    quarto: pathlib.Path
+    google_tracking_id: str
 
-    google_tracking_id = env.get("google_tracking_id", _google_tracking_id, required=True,)
-    dry = int(env.get("dry") or _dry)
+    def __init__(self, *, 
+         git_commit: str, git_ref: str,
+         google_tracking_id: str,
+         quarto: pathlib.Path = QUARTO, 
+         quarto_variables: pathlib.Path = QUARTO_VARIABLES,
+     ):
 
-    logger.debug("Loading `%s`.", quarto)
-    with open(quarto, "r") as file:
-        data = yaml.safe_load(file)
+        self.git_commit = git_commit
+        self.git_ref = git_ref
 
-    logger.debug("Updating `%s`.", quarto)
-    if (website := data.get("website")) is None:
-        print(f"`{QUARTO}` does not contain required key `website`.")
-        return 1
+        self.quarto = quarto
+        self.quarto_variables = quarto_variables
+        self.google_tracking_id = google_tracking_id
 
-    if website.get("google-analytics") is not None:
-        print(f"`{QUARTO}` already has a value for `website.google-analytics`.")
-        return 2
 
-    website["google-analytics"] = google_tracking_id
-    if dry:
-        print(yaml.dump(website))
+    def set_tracking_id(self, dry: bool) -> int:
+        logger.debug("Loading `%s`.", self.quarto)
+        with open(self.quarto, "r") as file:
+            data = yaml.safe_load(file)
+
+        logger.debug("Updating `%s`.", self.quarto)
+        if (website := data.get("website")) is None:
+            print(f"`{QUARTO}` does not contain required key `website`.")
+            return 1
+
+        if website.get("google-analytics") is not None:
+            print(f"`{QUARTO}` already has a value for `website.google-analytics`.")
+            return 2
+
+        website["google-analytics"] = self.google_tracking_id
+        if dry:
+            print("---")
+            print("# _quarto.yaml\n")
+            print(yaml.dump(website))
+            return 0
+
+        logger.debug("Dumping updated `%s`.", self.quarto)
+        with open(self.quarto, "w") as file:
+            yaml.safe_dump(data, file)
+
         return 0
 
-    logger.debug("Dumping updated `%s`.", quarto)
-    with open(quarto, "w") as file:
-        yaml.safe_dump(data, file)
+    def spawn_variables(self, dry: bool) -> int:
 
-    return 0
+        data =   {"build_git_commit": self.git_commit, "build_git_ref": self.git_ref,
+                  "build_timestamp": datetime.timestamp(datetime.now())}
+
+        if dry:
+            print("---")
+            print("# _variables.yaml\n")
+            print(yaml.dump(data))
+            return 0
+
+        with open(self.quarto_variables, "w") as file:
+            yaml.dump(data, file)
+
+        return 0
+
+
+def main(_google_tracking_id: str | None = None, _dry: str = "1",):
+
+    google_tracking_id: str = env.get("google_tracking_id", _google_tracking_id, required=True,) # type: ignore
+
+    git_commit : str = env.get("git_commit", required=True) # type: ignore
+    git_ref: str = env.get("git_ref", required=True) # type: ignore
+
+    dry = int(env.get("dry") or _dry) != 0
+
+    context = Context(google_tracking_id=google_tracking_id, git_commit=git_commit, git_ref=git_ref,)
+    if (out := context.set_tracking_id(dry)): 
+        return out
+
+
+    return context.spawn_variables(dry)
 
 
 if __name__ == "__main__":
